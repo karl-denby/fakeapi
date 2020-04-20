bb4427a82cc51a981e8f8aded7f7f9184317e0d8
karl-denby <karl.denby@gmail.com>
2020-04-20 16:18:59 +0100

    add test email endpoint

--- index.js
+++ index.js
@@ -12,5 +12,19 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/email', (req, res) => {
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: process.env.SENDGRID_EMAIL_TO,
+    from: process.env.SENDGRID_EMAIL_FROM,
+    subject: 'Sendgrid email',
+    text: 'triggered from express in Node.js',
+    html: '<strong>triggered from express in Node.js</strong>',
+  };
+  sgMail.send(msg);
+  res.send('Check your email')
+})
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
